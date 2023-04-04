f5ca9c9096e11e3e6574e6e3a0524b717d26b9c0
WallaceMartinz <wallace.mota@sptech.school>
2023-04-04 19:58:58 -0300

    Envio de dados para o banco

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -62,13 +62,16 @@ function entrar(req, res) {
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
+    var razao = req.body.razaoServer;
+    var cnpj = req.body.cnpjServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
 
     // Faça as validações dos valores
-    if (nome == undefined) {
+    if (razao == undefined) {
         res.status(400).send("Seu nome está undefined!");
+    } else if (cnpj == undefined){    
+        res.status(400).send("Seu Cnpj está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
@@ -76,7 +79,7 @@ function cadastrar(req, res) {
     } else {
         
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(razao, cnpj, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
